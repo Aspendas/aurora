78aed3aaa64aa0eea84b4d9b862b0d1ff4116dce
BBBakir <burakbak>
2023-02-13 01:53:26 +0300

    adjust screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'log_screen.dart';
 import 'register_screen.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 void main() {
   runApp(Aurora());
@@ -10,18 +9,11 @@ void main() {
 class Aurora extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ScreenUtilInit(
-      designSize: const Size(360, 690),
-      minTextAdapt: true,
-      splitScreenMode: true,
-      builder: (context, child) {
-        return MaterialApp(
-          initialRoute: LogScreen.id,
-          routes: {
-            LogScreen.id: (context) => LogScreen(),
-            RegScreen.id: (context) => RegScreen(),
-          },
-        );
+    return MaterialApp(
+      initialRoute: LogScreen.id,
+      routes: {
+        LogScreen.id: (context) => LogScreen(),
+        RegScreen.id: (context) => RegScreen(),
       },
     );
   }
