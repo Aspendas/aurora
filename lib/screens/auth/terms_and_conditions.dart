9e76f579f5d4a2f25c66909f563d24311377c6b1
Aspendas <umutyasin2001@gmail.com>
2023-02-19 22:24:59 +0300

    terms and condition added

---
+++ lib/screens/auth/terms_and_conditions.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TermAndConditions extends StatelessWidget {
+  const TermAndConditions({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
