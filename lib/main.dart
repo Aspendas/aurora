2d78d6decaff4d4c83cd979b78d5be164a00af68
Aspendas <umutyasin2001@gmail.com>
2023-03-29 11:12:34 +0300

    debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -21,6 +21,7 @@ class Aurora extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: const Wrapper(),
       routes: {
         LoginScreen.id: (context) => const LoginScreen(),
